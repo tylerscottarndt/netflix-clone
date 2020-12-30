d5d2f92b9a62d150c19284da028cb539e26a5e17
Tyler Arndt <Tyler.Arndt@motorolasolutions.com>
2020-12-30 14:10:20 -0600

    format netflix originals differently than the other rows

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     //state is the way to write variables in REACT
     //useState sets the 'movies' initial value (to the empty array)
     const [movies, setMovies] = useState([]);
@@ -36,8 +36,13 @@ function Row({ title, fetchUrl }) {
             <div className="row_posters">
                 {movies.map((movie) => (
                     <img
-                        className="row_poster"
-                        src={base_url + movie.poster_path}
+                        key={movie.id}
+                        className={`row_poster ${
+                            isLargeRow && 'row_posterLarge'
+                        }`}
+                        src={`${base_url}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path
+                        }`}
                         alt={movie.name}
                     ></img>
                 ))}
